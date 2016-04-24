0590b8b92d7b70ffacf46a223d9c7088c856322b
zhhlive@gmail.com <zhhlive@gmail.com>
2016-04-23 22:43:42 -0700

    Modified switchers listener and html data attrs

--- app_client/js/main.js
+++ app_client/js/main.js
@@ -1,6 +1,38 @@
 (function ($, L) {
     'use strict';
-    var defaultZoom = 16;
+    // Get the user's API key via prompt
+    if (!localStorage.getItem('uc_api_key') || localStorage.getItem('uc_api_key') == "null") {
+        localStorage.setItem('uc_api_key', "357C908148B64CD19F08");
+    }
+    // Get the user's API secret via prompt
+    if (!localStorage.getItem('uc_api_secret') || localStorage.getItem('uc_api_secret') == "null") {
+        localStorage.setItem('uc_api_secret', "0E6932BAC9A84E068B9A66DA4C8F3D53");
+    }
+    var defaultZoom = 16,
+        googleLayers = {
+          streets: L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+              maxZoom: 20,
+              subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+          }),
+          hybrid: L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}', {
+              maxZoom: 20,
+              subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+          }),
+          datellite: L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+              maxZoom: 20,
+              subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+          }),
+          terrain: L.tileLayer('http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}', {
+              maxZoom: 20,
+              subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+          })
+        },
+    // Confirm we've got 'em by displaying them to the screen
+        apiKey = localStorage.getItem('uc_api_key'),
+        apiSecret = localStorage.getItem('uc_api_secret'),
+        map, currentLayer,
+        $layerSwitcher = $('.layer-switcher');
+
 
     function start() {
         var lat, lng;
@@ -18,92 +50,60 @@
             // TODO use IP get location
             creatMap(lat, lng);
         }
-    };
+    }
 
     function creatMap(lat, lng) {
-        // Get the user's API key via prompt
-        if (!localStorage.getItem('uc_api_key') || localStorage.getItem('uc_api_key') == "null") {
-            localStorage.setItem('uc_api_key', "357C908148B64CD19F08");
-        }
-        // Get the user's API secret via prompt
-        if (!localStorage.getItem('uc_api_secret') || localStorage.getItem('uc_api_secret') == "null") {
-            localStorage.setItem('uc_api_secret', "0E6932BAC9A84E068B9A66DA4C8F3D53");
-        }
-        // Confirm we've got 'em by displaying them to the screen
-        var apiKey = localStorage.getItem('uc_api_key'),
-            apiSecret = localStorage.getItem('uc_api_secret');
-        // Create a Leaflet map
-        var map = L.map('map')
-            .setView([
-                lat,
-                lng
-            ], defaultZoom);
-        // Create a simple UC tile layer - global map, no restrictions
-        var url = 'https://tile-{s}.urthecast.com/v1/rgb/{z}/{x}/{y}?api_key=' + apiKey + '&api_secret=' + apiSecret;
-        // Append it to the map
-        // var ucTiles = L.tileLayer(url).addTo(map);
-        
-        var googleLayers = {};
-        googleLayers.googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
-            maxZoom: 20,
-            subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+      map = L.map('map').setView([lat, lng], defaultZoom);
+      // changeLayer(googleHybrid)();
+      map.addLayer(googleLayers.hybrid);
+      currentLayer = 'hybrid';
+      L.marker([lat, lng])
+          .addTo(map);
+      map.on('click', function (e) {
+        var latlng = e.latlng;
+        $.ajax({
+          method: 'GET',
+        url: '/api/getData?lat=' + latlng.lat + '&lng=' + latlng.lng
+        })
+        .done(function (data) {
+          // console.log(data);
+          var pop1 = '<div><strong>City: </strong><span>';
+          var pop2 = '</span><br><strong>Weather: </strong><span>';
+          var pop3 = '</span><br><strong>Tempurature: </strong><span>';
+          var pop4 = '</span><br><strong>Max Temp: </strong><span>';
+          var pop5 = '</span><br><strong>Min Temp: </strong><span>';
+          var pop6 = '</span></div>';
+          var popupTemplate = pop1 + data.city + pop2 + data.summary + pop3 + data.temperature + pop4 + data.daySummary.temperatureMax + pop5 + data.daySummary.temperatureMin + pop6;
+                // console.log(popupTemplate);
+          var popup = L.popup().setLatLng(latlng).setContent(popupTemplate).openOn(map);
         });
-        googleLayers.googleHybrid = L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}', {
-            maxZoom: 20,
-            subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
-        });
-        googleLayers.googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
-            maxZoom: 20,
-            subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
-        });
-        googleLayers.googleTerrain = L.tileLayer('http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}', {
-            maxZoom: 20,
-            subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
-        });
-        
-        function changeLayer(layer) {
-            return function() {
-                for (var l in googleLayers) {
-                    if (map.hasLayer(googleLayers[l])) {
-                        map.removeLayer(googleLayers[l]);
-                    }
-                }
-                layer.addTo(map);
-            };
-        }
-        $('#streets-btn').on('click', changeLayer(googleLayers.googleStreets));
-        $('#hybrid-btn').on('click', changeLayer(googleLayers.googleHybrid));
-        $('#sat-btn').on('click', changeLayer(googleLayers.googleSat));
-        $('#terrain-btn').on('click', changeLayer(googleLayers.googleTerrain));
-        
-        // changeLayer(googleHybrid)();
-        map.addLayer(googleLayers.googleHybrid);
-        L.marker([lat, lng])
-            .addTo(map);
-        map.on('click', function (e) {
-            var latlng = e.latlng;
-            $.ajax({
-                method: 'GET',
-                url: '/api/getData?lat=' + latlng.lat + '&lng=' + latlng.lng
-            })
-                .done(function (data) {
-                    // console.log(data);
-                    var pop1 = '<div><strong>City: </strong><span>';
-                    var pop2 = '</span><br><strong>Weather: </strong><span>';
-                    var pop3 = '</span><br><strong>Tempurature: </strong><span>';
-                    var pop4 = '</span><br><strong>Max Temp: </strong><span>';
-                    var pop5 = '</span><br><strong>Min Temp: </strong><span>';
-                    var pop6 = '</span></div>';
-                    var popupTemplate = pop1 + data.city + pop2 + data.summary + pop3 + data.temperature + pop4 + data.daySummary.temperatureMax + pop5 + data.daySummary.temperatureMin + pop6;
-                    // console.log(popupTemplate);
-                    var popup = L.popup()
-                        .setLatLng(latlng)
-                        .setContent(popupTemplate)
-                        .openOn(map);
-                });
-        });
-    };
+      });
+    }
 
-    $('document')
-        .ready(start());
+    function changeLayer(layer) {
+        // return function() {
+        //     for (var l in googleLayers) {
+        //         if (map.hasLayer(googleLayers[l])) {
+        //             map.removeLayer(googleLayers[l]);
+        //         }
+        //     }
+        //     layer.addTo(map);
+        // };
+      map.removeLayer(googleLayers[currentLayer]);
+      map.addLayer(googleLayers[layer]);
+      currentLayer = layer;
+    }
+
+    $layerSwitcher.on('click', function(e) {
+      var $target = $(e.target);
+      $(this).children('.btn-info').removeClass('btn-info').addClass('btn-primary');
+      $target.addClass('btn-info');
+      changeLayer($target.data('layer'));
+    });
+    // $('#streets-btn').on('click', changeLayer(googleLayers.googleStreets));
+    // $('#hybrid-btn').on('click', changeLayer(googleLayers.googleHybrid));
+    // $('#sat-btn').on('click', changeLayer(googleLayers.googleSat));
+    // $('#terrain-btn').on('click', changeLayer(googleLayers.googleTerrain));
+
+    $('document').ready(start());
 } ($, L));
